ea48ef036ecfbe6c0c2917f174a1fcdcec4f6079
EsraAlhaddad <israalhaddad9812@gmail.com>
2023-06-17 14:43:07 +0300

    yeni ekleme yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:traveller/screens/home_bottom_bar_screens/home_screen.dart';
 import 'package:traveller/screens/app_screens/onbording_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Firebase.initializeApp();
   await Hive.initFlutter();
   await Hive.openBox("Bookmark");
