105098699ed186ad0e070f58bb42c18c1a28c926
EsraAlhaddad <israalhaddad9812@gmail.com>
2023-06-20 17:13:56 +0300

    add

--- lib/screens/app_screens/welcome_screen.dart
+++ lib/screens/app_screens/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:traveller/widget/home/home_bottom_bar_widget.dart';
 
 class WelcomeScreen extends StatefulWidget {
